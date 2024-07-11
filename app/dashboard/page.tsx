46e00c559368543690ea1bcc7cc9c179bfd9f432
ayussh-2 <ayushatschool5@gmail.com>
2024-07-11 23:01:25 +0530

    updated working apollo client

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,51 +1,20 @@
 "use client";
 
 import { Button } from "flowbite-react";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 import { useContext, useEffect } from "react";
-import { useQuery } from "@apollo/client";
-import { orgId } from "@/staticData/gqVars";
-import { GET_EVENTS } from "@/graphql/queries/getEvents";
+
 import { AuthContext } from "@/context/AuthContext";
 
 export default function Home() {
     const { handleSignOut, userInfo } = useContext(AuthContext);
-    const router = useRouter();
-    const {
-        data: eventsData,
-        loading: eventsLoading,
-        error: eventsError,
-        refetch: refetchEvents,
-    } = useQuery(GET_EVENTS, {
-        variables: {
-            orgId: orgId,
-        },
-    });
-
-    const navigateToUserDashboard = () => {
-        router.push("/dashboard/users");
-    };
-
-    const navigateToTeamsDashboard = () => {
-        router.push("/dashboard/teams");
-    };
 
     useEffect(() => {
-        if (!userInfo.uid) {
+        const authToken = localStorage.getItem("auth-token");
+        if (!authToken) {
             window.location.href = "/";
         }
     }, [userInfo]);
-
-    useEffect(() => {
-        if (eventsData) {
-            console.log("eventsData", eventsData);
-        }
-        if (eventsError) {
-            console.log("eventsError", eventsError);
-        }
-    }, [eventsData, eventsError]);
-
-    if (eventsLoading) return <div>Loading...</div>;
 
     return (
         <div className="flex justify-center items-center h-screen">
@@ -56,20 +25,21 @@ export default function Home() {
                 <div className="mb-4 text-3xl text-center">
                     Click on the buttons to visit the respective dashboard.
                 </div>
-                <Button
-                    color="gray"
-                    className="w-2/5 mb-3"
-                    onClick={navigateToUserDashboard}
-                >
-                    Go to Users Dashboard
-                </Button>
-                <Button
-                    color="gray"
-                    className="w-2/5 mb-3"
-                    onClick={navigateToTeamsDashboard}
-                >
-                    Go to Teams Dashboard
-                </Button>
+                <Link href="/dashboard/users">
+                    <Button color="gray" className="mb-3">
+                        Go to Users Dashboard
+                    </Button>
+                </Link>
+                <Link href="/dashboard/teams">
+                    <Button color="gray" className="mb-3">
+                        Go to Teams Dashboard
+                    </Button>
+                </Link>
+                <Link href="/dashboard/sampleQuery">
+                    <Button color="gray" className="mb-3">
+                        Go to Sample Query Page
+                    </Button>
+                </Link>
             </div>
         </div>
     );
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Toaster } from "react-hot-toast";
 import { AuthProvider } from "@/context/AuthContext";
 import { ApolloProvider } from "@apollo/client";
 import client from "@/lib/apollo-client";
+import NextTopLoader from "nextjs-toploader";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,11 @@ export default function RootLayout({
         <html lang="en">
             <title>SRC Dashboard</title>
             <body className={inter.className}>
+                <NextTopLoader
+                    color="#2563EB"
+                    showSpinner={false}
+                    easing="ease-in"
+                />
                 <ApolloProvider client={client}>
                     <AuthProvider>
                         {children}
