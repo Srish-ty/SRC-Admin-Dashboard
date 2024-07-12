a518039955b929010fefa6a74ea6be292e0289b6
ayanSCYY <ayanm3206@gmail.com>
2024-07-12 07:09:42 +0530

    feat:add changes

--- app/dashboard/teams/page.tsx
+++
@@ -1,9 +0,0 @@
-
-
-import MainTeamComponent from "@/components/MainTeamComponent";
-
-export default function Component() {
-  return(
-    <MainTeamComponent/>
-  )
-}
--- staticData/staticData.ts
+++ staticData/staticData.ts
@@ -1,3 +1,3 @@
 export const RoutePageData=[
-    {route:"Go to Users Dashboard",uri:"/users"},{route:"Go to Teams Dashboard",uri:`/teams/${"CHEM-E-CAR"}`},{route:"Go to Sample Query Page",uri:"/SampleQuery"}
+    {route:"Go to Users Dashboard",uri:"/users"},{route:"Go to Teams Dashboard",uri:`/teams/${"CHEM-E-CAR"}`}
 ]
